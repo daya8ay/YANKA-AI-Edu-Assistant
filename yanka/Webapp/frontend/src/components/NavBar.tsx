f49eca0c9342ef41e61a2d23a635057e01fc585a
Guri080 <gursparshsodhi@gmail.com>
2025-10-27 01:55:09 -0700

    created a support page

--- yanka/Webapp/frontend/src/components/NavBar.tsx
+++ yanka/Webapp/frontend/src/components/NavBar.tsx
@@ -19,6 +19,7 @@ export default function NavBar() {
       <div className={styles.navLinks}>
         <Link href="/">Home</Link>
         <Link href="/avatar_creation">Avatar Creation</Link>
+        <Link href="/support">Support</Link>
       </div>
     </nav>
   );
