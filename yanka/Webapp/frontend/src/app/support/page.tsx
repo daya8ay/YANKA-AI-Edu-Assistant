f44415649dd965d196a6286303192a81381c516a
Guri080 <gursparshsodhi@gmail.com>
2025-11-04 23:23:37 -0700

    font changes. first image is hyperlinked

--- yanka/Webapp/frontend/src/app/support/page.tsx
+++ yanka/Webapp/frontend/src/app/support/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './support.module.css';
 import NavBar from '@/components/NavBar';
-// import styles from '../page.module.css';
+import Link from "next/link";
 
 export const metadata = {
   title: 'Customer Support - YANKA',
@@ -12,7 +12,7 @@ const SupportPage = () => {
   return (
     <>
       <NavBar />
-      <div className={styles.page}>
+      <div className={styles.supportContainer}>
         <main className={styles.main}>
           <h2 className={styles.leftText}>How can we help?</h2>
           <input
@@ -21,25 +21,27 @@ const SupportPage = () => {
             className={styles.searchBox}
           />
         </main>
-          <div className={styles.suggestions}>
-            <h3>New to Yanka?</h3>
-            {/* You can add more content here */}
-        </div>
-        <div className={styles.imageRow}>
-        <div className={styles.imageItem}>
-          <img src="/pics/support-img1.jpg" alt="Image 1" />
-          <p>Getting started with Yanka</p>
-        </div>
-        <div className={styles.imageItem}>
-          <img src="/pics/support-img3.jpg" alt="Image 2" />
-          <p>Video Tutorials</p>
-        </div>
-        <div className={styles.imageItem}>
-          <img src="/pics/support-img2.webp" alt="Image 3" />
-          <p>Navigating the Yanka interface</p>
-        </div>
-      </div>
 
+        <div className={styles.suggestions}>
+          <h3>New to Yanka?</h3>
+        </div>
+
+        <div className={styles.imageRow}>
+          <div className={styles.imageItem}>
+            <Link href="/#pricing">
+              <img src="/pics/support-img1.jpg" alt="Image 1" />
+            </Link>
+            <p>Getting started with Yanka</p>
+          </div>
+          <div className={styles.imageItem}>
+            <img src="/pics/support-img3.jpg" alt="Image 2" />
+            <p>Video Tutorials</p>
+          </div>
+          <div className={styles.imageItem}>
+            <img src="/pics/support-img2.webp" alt="Image 3" />
+            <p>Navigating the Yanka interface</p>
+          </div>
+        </div>
       </div>
     </>
   );
