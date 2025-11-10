75c6007dcb4098602932733acd116166ac46c14b
Guri080 <gursparshsodhi@gmail.com>
2025-11-10 16:37:05 -0700

    Added a chat box feature

--- yanka/Webapp/frontend/src/app/support/page.tsx
+++ yanka/Webapp/frontend/src/app/support/page.tsx
@@ -1,50 +1,19 @@
-import React from 'react';
-import styles from './support.module.css';
 import NavBar from '@/components/NavBar';
-import Link from "next/link";
+import SupportContent from './SupportContent';
+import styles from './support.module.css';
 
 export const metadata = {
   title: 'Customer Support - YANKA',
   description: 'Get help and support for your account',
 };
 
-const SupportPage = () => {
+export default function SupportPage() {
   return (
     <>
       <NavBar />
       <div className={styles.supportContainer}>
-        <main className={styles.main}>
-          <h2 className={styles.leftText}>How can we help?</h2>
-          <input
-            type="text"
-            placeholder="Search how-tos and more"
-            className={styles.searchBox}
-          />
-        </main>
-
-        <div className={styles.suggestions}>
-          <h3>New to Yanka?</h3>
-        </div>
-
-        <div className={styles.imageRow}>
-          <div className={styles.imageItem}>
-            <Link href="/#pricing">
-              <img src="/pics/support-img1.jpg" alt="Image 1" />
-            </Link>
-            <p>Getting started with Yanka</p>
-          </div>
-          <div className={styles.imageItem}>
-            <img src="/pics/support-img3.jpg" alt="Image 2" />
-            <p>Video Tutorials</p>
-          </div>
-          <div className={styles.imageItem}>
-            <img src="/pics/support-img2.webp" alt="Image 3" />
-            <p>Navigating the Yanka interface</p>
-          </div>
-        </div>
+        <SupportContent />
       </div>
     </>
   );
-};
-
-export default SupportPage;
+}
